aeccff374fc5e2c55c76ce2b481af589ea98e0f4
Aqil <aqilaswangga1@gmail.com>
2023-12-03 12:50:57 +0700

    adding some files

---
+++ lib/guide_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'theme.dart';
+class GuidePage extends StatefulWidget {
+  const GuidePage({super.key});
+
+  @override
+  State<GuidePage> createState() => _Guid_pageePate();
+}
+
+class _Guid_pageePate extends State<GuidePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color primarycolor = const Color.fromARGB(255, 52, 86, 208);
+Color background1 = const Color.fromARGB(255, 19, 30, 70);
+Color secondarycolor = const Color.fromARGB(255, 64, 82, 146);
+Color lightcolor = const Color.fromARGB(255, 120, 149, 255);
